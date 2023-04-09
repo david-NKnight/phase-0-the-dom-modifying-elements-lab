c14ded834e8d02eebb1cf12a1a405c595bc5ab1b
davidknight <davidb_nunes@yahoo.com.br>
2023-04-09 10:34:41 -0500

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+function createHeaderWithText(text) {
+    const main = document.querySelector('main#main');
+    main.parentNode.removeChild(main);
+  
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.innerHTML = `${text} is the champion`;
+  
+    document.body.appendChild(newHeader);
+  
+    return newHeader;
+  }
+  
+  const newHeader = createHeaderWithText('YOUR-NAME');
